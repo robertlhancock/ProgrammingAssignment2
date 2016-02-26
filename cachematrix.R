44122467e408fe352e5ff3fb212310ca4222c8f9
robertlhancock <rh4991@att.com>
2016-02-26 12:00:09 -0500

    resubmitting assignment
    After reading peer review feedback I realized that my submission did not accurately enough comply with the instruction.
    In this revision:
    1. create an indicator variable and a test for isMatrixInverted
    2. move solve into cachesolve()
    3. add testing to cachesolve() to determine if the matrix is already inverted. If it is not inverted then run solve() to do that before returning the cached value.

--- cachematrix.R
+++ cachematrix.R
@@ -1,84 +1,113 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 
 ## Upon calling makeCacheMatrix( x = matrix()) with a
-## invertable matrix the value of the inverse will be stored for later retrieval.
+## invertable matrix the value of the matrix will be stored for later retrieval.
 ## the ability to retrieve stored information is part of the exercise that demonstrates
 ## R's method of caching results of costly data operations where the dataset is large
 ## and the operation is costly.
 
 ## In the example below there exits a 3 by 3 invertable matrix identified by b.
-## b is passed into the makeCacheMatrix function and its inverse is stored internal to the 
+## b is passed into the makeCacheMatrix function and is stored internal to the 
 ## resulting object abcd.
-## abcd$y can be called to retrieve the value of the stored data.
-## Also abcd$get() can be called to retrieve the value of the stored data.
-## Conversely abcd$set( x) can be called to save another matrices inversion data to be retrieved later.
+## 
+## ## b <- matrix( c( 1,0,4,1,3,4,4,1,0), 3, 3)
+## > abcd <- makeCacheMatrix(b)
+## > abcd$getMatrix()
+##      [,1] [,2] [,3]
+## [1,]    1    1    4
+## [2,]    0    3    1
+## [3,]    4    4    0
+## > abcd$isInverted()
+## [1] FALSE
+## > cacheSolve(abcd)
+##             [,1]       [,2]        [,3]
+## [1,]  0.08333333 -0.3333333  0.22916667
+## [2,] -0.08333333  0.3333333  0.02083333
+## [3,]  0.25000000  0.0000000 -0.06250000
+## > abcd$getMatrix()
+##             [,1]       [,2]        [,3]
+## [1,]  0.08333333 -0.3333333  0.22916667
+## [2,] -0.08333333  0.3333333  0.02083333
+## [3,]  0.25000000  0.0000000 -0.06250000
+## > abcd$isInverted()
+## [1] TRUE
+
+## abcd$isInverted can be called to retrieve an indicator that the matrix stored is the inverted value.
+## 
+## abcd$getMatrix() can be called to retrieve the value of the stored data.
+## 
+## Conversely abcd$setInvertedMatrix( x) can be called to save another matrices inversion data to be retrieved later.
+## setInvertedMatrix(x) will clear the inverted indicator to TRUE
 ## 
 
-## > b
-##   [,1] [,2] [,3]
-##   [1,]    1    0    4
-##   [2,]    1    3    4
-##   [3,]    4    1    0
-##> abcd <- makeCacheMatrix(b)
-##
-##> abcd$get()
-##  ##  [,1]        [,2]    [,3]
-##  [1,]  0.08333333 -0.08333333  0.2500
-##  [2,] -0.33333333  0.33333333  0.0000
-##  [3,]  0.22916667  0.02083333 -0.0625
-##
-## create a new matrix named a by multiplying b * 10
-## a<-b*10
-##  > a
-##  [,1] [,2] [,3]
-##  [1,]   10    0   40
-##  [2,]   10   30   40
-##  [3,]   40   10    0
-##
-##
-##> save the inverse of a
-##> abcd$set(a)
-##
-##  get the inverse of a
-##> abcd$get()
-##  [,1]         [,2]          [,3]
-##  [1,]  0.008333333 -0.008333333  2.500000e-02
-##  [2,] -0.033333333  0.033333333 -2.018587e-18
-##  [3,]  0.022916667  0.002083333 -6.250000e-03
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
-  ## Step 1 save the inverse of matrix x using solve()
-  y <<- solve(x)
+  ## Step 1 save the matrix x and set the inverted indicator
+  isInvertedInd <<- FALSE
+  thisMatrix <<- x
   
-  ## Step 2 create a set function that takes as its argument
+  ## Step 2 create a setMatrix function that takes as its argument
   ## matrix x and saves the inverse of it using solve().
   ## ## NOTE: This method is added for completeness and testing
-  set <- function(x) {
-    y <<- solve(x)
+  setMatrix <- function(x) {
+    thisMatrix <<- x
+    isInvertedInd <<- FALSE
   
-  }
-  
-  ## Step 3 create a get function that will return the value of inverse
-  ## of matrix x. 
-  get <- function() {
-    y
   }
   
 
+  ## Step 3 create a getMatrix function that will return the value of the matrix
+  ## of matrix x. 
+  getMatrix <- function() {
+    thisMatrix
+  }
   
-  ## Step 4, create a list of the objects exposed and return them to be 
+  ## Step 4. set an inverted matrix
+  setInvertedMatrix <- function( x){
+    thisMatrix <<- x
+    isInvertedInd <<- TRUE
+  }
+  
+  ## Step 5. test the inverted indicator
+  isInverted <- function(){
+    return(isInvertedInd)
+  }
+  
+  ## Step 6, create a list of the objects exposed and return them to be 
   ## used in subsetting the resulting function
-  ## we will expose the set, get and the matrix itself
-  list(set = set, 
-       get = get)
+  ## we will expose the setMatrix, getMatrix, setInvertedMatrix and the inverted indicator itself
+  list(setMatrix = setMatrix,
+       setInvertedMatrix = setInvertedMatrix, 
+       getMatrix = getMatrix,
+       isInverted = isInverted
+       )
 
 }
 
+## cacheSolve: Perform an operation on a cached matrix to solve it if necessary.
+## 1. Check to see if the cache matrix is inverted. 
+## 2. If the matrix is inverted return it
+## 3. otherwise get the cached matrix.
+## 4. call solve() on the matrix to invert it.
+## 5. save the resulting inverted matrix back to the cache
+## 6. return the inverted matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  data <- x$get()
+  
+  ## 1. Check to see if "x" is inverted.
+  if(x$isInverted()) {
+    ## 2. return inverted matrix
+     return( x$getMatrix())
+  }
+  ## 3. get the cached matrix
+  data <- x$getMatrix()
+  
+  ## 4. use solve() to get the matrix inverted.
+  m <- solve(data, ...)
+  
+  ## 5. cache the inverted matrix back to "x"
+  x$setInvertedMatrix(m)
+  
+  ## 6. return the inverted matrix
+  return( m)
 }
